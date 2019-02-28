82db5737116cea6cc28644ce38e02231ac8ccbc7
Planet15 <parmar0815@gmail.com>
2019-02-27 22:38:54 -0500

    getting started

---
+++ Assets/JavaScript/game.js
@@ -0,0 +1,9 @@
+
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var words = ['narcos', 'house of cards', 'abstract', 'you', 'mindhunter', 'ozark', 'dear white peopel', 'master of none']
+var numberGuesses = 5;
+var guessChoices = [];
+var wins = 0;
+var losses = 0;
