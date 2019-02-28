3b8355222ad3f28b84cdb4e5da925ac196806106
Planet15 <parmar0815@gmail.com>
2019-02-28 17:10:01 -0500

    added comments

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,28 +1,23 @@
-
+// these are the characters the user can press on the keybord to guess the letter.
 var alphabetChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
+// this is making the computer guess a letter from the alphabetChoices variable. 
 var computerGuess = alphabetChoices[Math.floor(Math.random() * alphabetChoices.length)];
+// these are all variables for different things the user see. These are also used in js for the logic of the game.
 var wins = 0;
 var losses = 0;
 var userChoicesEmpty = [];
 var numberGuesses = 5;
 
-// // this is what the comp uses to select from the array
-// var grabbingRandomAlphabet = function (array) {
-//     array[Math.floor(Math.random() * array.length)];
-// }
-// // here it selects a word form the "word" array
-// var randomAlphabet = grabbingRandomAlphabet(alphabetChoices);
-// console.log(randomAlphabet);
 
+// these varaible are used to link it back into HTML
 var userGuessText = document.getElementById("userguess-text");
 var numberGuessesText = document.getElementById("numberguesses-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 
-// console.log(computerGuess);
+// this is recording the users key presses, and showing the outcome. (if the user guesses the correct answer and wins or he looses.)
 document.onkeyup = function (event) {
     var userGuess = event.key;
 
@@ -45,10 +40,10 @@ document.onkeyup = function (event) {
         numberGuesses = 5;
         userChoicesEmpty = [];
     }
-
+// this is showing the key the user pressed
     userChoicesEmpty.push(event.key);
 
-
+// this is helping to link it back to HTML
     userGuessText.textContent = "Your guess: " + userChoicesEmpty;
     numberGuessesText.textContent = "Guesses Left: " + numberGuesses;
     winsText.textContent = "Wins: " + wins;
