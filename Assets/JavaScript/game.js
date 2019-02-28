e1737e225f2e826a8bd1f8f674ec47cb46f99954
Planet15 <parmar0815@gmail.com>
2019-02-28 16:18:07 -0500

    got it to work

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,31 +1,62 @@
 
 var alphabetChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-// var words = ['narcos', 'house of cards', 'abstract', 'you', 'mindhunter', 'ozark', 'dear white people', 'master of none']
-var numberGuesses = 5;
-var guessChoices = [];
+
+var computerGuess = alphabetChoices[Math.floor(Math.random() * alphabetChoices.length)];
 var wins = 0;
 var losses = 0;
+var userChoicesEmpty = [];
+var numberGuesses = 5;
 
-// list of variable that will be taking the place in the HTML 
-// if/else statements to compare the letter with user and computer
-// guesses, wins, and losses update 
-// guesses so far by the user
-// 
+// // this is what the comp uses to select from the array
+// var grabbingRandomAlphabet = function (array) {
+//     array[Math.floor(Math.random() * array.length)];
+// }
+// // here it selects a word form the "word" array
+// var randomAlphabet = grabbingRandomAlphabet(alphabetChoices);
+// console.log(randomAlphabet);
 
-document.onkeyup - function(event) {
+var userGuessText = document.getElementById("userguess-text");
+var numberGuessesText = document.getElementById("numberguesses-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+
+// console.log(computerGuess);
+document.onkeyup = function (event) {
     var userGuess = event.key;
+
+    if (userGuess === computerGuess) {
+        wins++;
+        numberGuesses = 5;
+        userChoicesEmpty = [];
+        computerGuess = alphabetChoices[Math.floor(Math.random() * alphabetChoices.length)];
+    }
+
+    else if (userGuess != computerGuess) {
+        
+        numberGuesses--;
+        
+    }
+
+    if (numberGuesses === 0) {
+        losses++;
+        computerGuess = alphabetChoices[Math.floor(Math.random() * alphabetChoices.length)];
+        numberGuesses = 5;
+        userChoicesEmpty = [];
+    }
+
+    userChoicesEmpty.push(event.key);
+
+
+    userGuessText.textContent = "You chose: " + userChoicesEmpty;
+    numberGuessesText.textContent = "Guesses Left: " + numberGuesses;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+
 }
 
-// this is what the comp uses to select from the array
-var grabbingRandomShow = function(array) {
-    array[Math.floor(Math.random() * array.length)];
-}
-// here it selects a word form the "word" array
-var randomShow = grabbingRandomShow(alphabetChoices);
-console.log ('randomShow', randomShow);
 
-if (userGuess < 1) {
-    
-}
+
+
+
